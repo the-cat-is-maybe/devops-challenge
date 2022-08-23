4c4c2e61944dd1d3edca5a5d91d68fc9cbba11b3
Jason Verde <Jason.e.verde@gmail.com>
2022-08-23 04:27:08 -0400

    Added a proposed iteration of the api, allowing for multiple field searching while keeping the number of functions and modifications required low.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from bson.objectid import ObjectId
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
 
-from src.mongoflask  import MongoJSONEncoder, ObjectIdConverter, find_restaurants
+from src.mongoflask  import MongoJSONEncoder, ObjectIdConverter, find_restaurants_2
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = environ.get("MONGO_URI")
@@ -16,14 +16,24 @@ mongo = PyMongo(app)
 
 @app.route("/api/v1/restaurant")
 def restaurants():
-    restaurants = find_restaurants(mongo)
+    restaurants = find_restaurants_2(mongo)
     return jsonify(restaurants)
-
 
 @app.route("/api/v1/restaurant/<id>")
 def restaurant(id):
-    restaurants = find_restaurants(mongo, id)
-    return jsonify(restaurants)
+    restaurants = find_restaurants_2(mongo, "_id", ObjectId(id))
+    if restaurants == {}:
+        return '', 204
+    else:
+        return jsonify(restaurants)
+
+@app.route("/api/v2/restaurant/<field>/<search>")
+def restaurant_wide_search(field,search):
+    restaurants = find_restaurants_2(mongo, field, search)
+    if restaurants == {}:
+        return '', 204
+    else:
+        return jsonify(restaurants)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=False, port=8080)
--- src/mongoflask.py
+++ src/mongoflask.py
@@ -4,6 +4,8 @@ import isodate as iso
 from bson import ObjectId
 from flask.json import JSONEncoder
 from werkzeug.routing import BaseConverter
+import re
+import urllib.parse
 
 
 class MongoJSONEncoder(JSONEncoder):
@@ -23,8 +25,29 @@ class ObjectIdConverter(BaseConverter):
     def to_url(self, value):
         return str(value)
 
-def find_restaurants(mongo, _id=None):
+##!## This next block is deprecated and should be considered for removal.
+## def find_restaurants(mongo, _id=None):
+##    query = {}
+##    if _id:
+##        query["_id"] = ObjectId(_id)
+##    result = mongo.db.restaurant.find(query)
+##    result_count = len(list(result.clone()))
+##    if result_count > 1:
+##        return list(result)
+##    else:
+##        return next(result, {})
+
+def find_restaurants_2(mongo, field=None, search=None):
     query = {}
-    if _id:
-        query["_id"] = ObjectId(id)
-    return list(mongo.db.restaurant.find(query))
+    if field:
+        if type(search) == str:
+            search = urllib.parse.unquote(search)
+            search = re.compile(search+'.*', re.IGNORECASE)
+        query[urllib.parse.unquote(field)] = search 
+    print (query)
+    result = mongo.db.restaurant.find(query)
+    result_count = len(list(result.clone()))
+    if result_count > 1:
+        return list(result)
+    else:
+        return next(result, {})
